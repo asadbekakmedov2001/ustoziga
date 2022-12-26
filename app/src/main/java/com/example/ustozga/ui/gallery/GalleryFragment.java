fa52c59d3751980c88aa0df714cb685f620e026f
Asadbek Axmedov <100250308+asadbekakmedov2001@users.noreply.github.com>
2022-12-26 10:57:10 +0500

    Update GalleryFragment.java

--- app/src/main/java/com/example/ustozga/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/ustozga/ui/gallery/GalleryFragment.java
@@ -48,14 +48,26 @@ public class GalleryFragment extends Fragment implements UserAdapter.SelectedUse
     }
     private void initial() {
 
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"1-video. Perspektiv tasvir qurish usullari","Perspektiv tasvir qurish usullari borasida to'liq...","MGMMOQEjSFA"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"2-video. Eng yaxshi ko'rish burchagi","Eng yaxshi ko'rish burchagi mavzusida qiziqarli faktlar...","D3m06DuLJaE"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"3-video. Perspektiv masshtablar 1-qism","Perspektiv masshtablar bilan ishlash borasida...","E7TwrzcCAT4"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"4-video. Perspektiv masshtablar 2-qism","Perspektiv masshtablar bilan ishlash borasida...","7tKa9mXFMhQ"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"5-video. Qopqog'i ochilgan quti perspektivasi","Qopqog'i ochilgan quti perspektivasi ...","DVE4Y0VEDJg"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"6-video. Arxitektorlar usuli","Loyihalash amaliyotda o‘zining yasalishi jixatidan soddaligi sababli keng tarqalgan perspektiva...","NXsXxgGgyuM"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"7-video. Radial nurlar izi usuli","Radial nurlar izi usuli ...","pkQis5dYhBs"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.video,"8-video. To'rlar usuli","Mikrorayon,  xiyobonlar,  sport  komplekslari  va  shu  kabi  katta  maydonlarga  ega  bo'lgan  inshootlarning  perspektivasini  yasashda  to 'r  usuli  eng  qulay  usul hisoblanadi...","4rRVEboObZI"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"1-video. Number Theory | Divisibility Basics","We present some basics of divisibility from elementary number theory...","Wg-JlvBVPi0"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"2-video. Number Theory: The Division Algorithm","In this video, we present a proof of the division algorithm and some examples of it in practice....","qEaxFxUK-es"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"3-video. Number Theory: The Euclidean Algorithm Proof","We present a proof of the Euclidean algorithm...","8cikffEcyPI"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"4-video. Number Theory: The Euclidean Algorithm Example 1","We compute the gcd of two numbers using the Euclidean algorithm...","_EGMgO1F4RI"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"5-video. Number Theory: The Euclidean Algorithm Example 2","We compute the gcd of two numbers using the Euclidean algorithm...","x935qXueQnA"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"6-video. Number Theory | Extended Euclidean Algorithm Example #1","We use the extended Euclidean algorithm to write the greatest common divisor of two natural numbers as a linear combination of them...","f1wP28zeM3w"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"7-video. Number Theory | Extended Euclidean Algorithm Example 2","We use the extended Euclidean algorithm to write the greatest common divisor of two natural numbers as a linear combination of them...","jywBOj-0r2E"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"8-video. Number Theory | Linear Diophantine Equations","We explore the solvability of the linear Diophantine equation ax+by=c...","ZA5FC9ONO3A"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"9-video. Number Theory | Primitive Pythagorean Triples","We derive the structure of all primitive Pythagorean triples...","F3dR41ItmSg"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"10-video. Number Theory | Congruence Modulo n -- Definition and Examples","We define the notion of congruence modulo n among the integers...","QjTMbspAjsw"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"11-video. Number Theory | Congruence and Equivalence Classes","We prove the congruence modulo n is an equivalence relation on the set of integers and describe the equivalence classes...","BU4eHTTgoCw"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"12-video. Number Theory | Some properties of integer congruence"," We examine some basic properties of congruence modulo n among the integers...","33p04rolbHA"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"13-video. Number Theory | A property of integer congruence","We prove a certain property of integer congruence and provide some examples...","F0QOs9MUfnc"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"14-video. Number Theory | Integer Congruence Example 1"," We give a few examples involving integer congruence...","mNBCv_UU47o"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"15-video. Number Theory | Integer Congruence Example 2"," We give a few examples involving integer congruence and simplification, without using Fermat's little Theorem, Euler's theorem, or Wilson's theorem...","1DbkGWBYQNM"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"16-video. Number Theory | When does a linear congruence have a solution??","We give the proof of a proposition regarding linear congruences and their solvability...","ktJc8_3pKPw"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"17-video. Number Theory | Linear Congruences Proposition 2","We give the proof of a proposition regarding the number of solutions of a linear congruence...","Ob54H1oNyBA"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"18-video. Number Theory | Strategies for Solving Linear Congruence","We outline a strategy for solving linear congruences and give an example...","5JwbEQY8lvk"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"19-video. Number Theory | Inverses modulo n","We give a characterization of numbers which are invertible modulo n...","15oQQbAnr3Q"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.video,"20-video. Number Theory | Modular Inverses: Example","We give an example of calculating inverses modulo n using two separate strategies...","uPFh9_nLw1c"));
 
     }
 
